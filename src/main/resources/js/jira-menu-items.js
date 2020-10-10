51c415f1c1248ce68c730db508ecbdded06c6656
Den <shd@mango.rocks>
2020-10-10 12:17:06 +0300

    AJS realisation

---
+++ src/main/resources/js/jira-menu-items.js
@@ -0,0 +1,19 @@
+/*
+AJS.$(document).on('ready', function(){
+    /!** начнёт работу тогда, когда будет готов DOM, за исключением картинок **!/
+
+    AJS.messages.info("#field-group", {
+        title: 'This is a title in a default message.',
+        body: '<p> And this is just content in a Default message.</p>'
+    });
+});*/
+
+$(function() {
+    /** начнёт работу тогда, когда будет готов DOM, за исключением картинок **/
+    AJS.messages.info("#field-group", {
+        title: 'This is a title in a default message.',
+        body: '<p> And this is just content in a Default message.</p>'
+    });
+});
+console.log('test msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msgtest msg');
+//alert('123');
